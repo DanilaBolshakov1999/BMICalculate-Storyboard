5665c020d23dee53efc738bce018d8c4a084aacd
Danila Bolshakov <danila1999777@gmail.com>
2023-08-01 10:46:33 +0300

    added application interface

---
+++ BMICalculate-Storyboard/View Controller/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BMICalculate-Storyboard
+//
+//  Created by iOS - Developer on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
