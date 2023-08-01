e9424bbd8029c216254564fd3a27e4bf5e8ce605
Danila Bolshakov <danila1999777@gmail.com>
2023-08-01 12:25:18 +0300

    added functionality for bmi application

--- BMICalculate-Storyboard/View Controller/ViewController.swift
+++ BMICalculate-Storyboard/View Controller/ViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var heightLabel: UILabel!
+    @IBOutlet var weightLabel: UILabel!
+    @IBOutlet var heightSlider: UISlider!
+    @IBOutlet var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        let height = String(format: "%.2f", sender.value)
+        heightLabel.text = "\(height) m"
+    }
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        let weight = String(format: "%.2f", sender.value)
+        weightLabel.text = "\(weight) kg"
+    }
+    
+    @IBAction func calculateButton(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        let bmi = weight / (height * height)
+        print(bmi)
     }
 }
 
